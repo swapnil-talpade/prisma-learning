c3c926f3f21fb7a59e98b5d29121cc3c6b630470
swapnil-talpade <swapniltalpade41@gmail.com>
2023-06-24 17:57:32 +0530

    chore: added enum and migrations

--- script.ts
+++ script.ts
@@ -2,9 +2,7 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const user = await prisma.user.findMany();
-
-  console.log(user);
+  const user = await prisma.user.deleteMany();
 }
 
 main()
