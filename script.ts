acb4ce9e1556051016ca259c4e797631e36edc1e
swapnil-talpade <swapniltalpade41@gmail.com>
2023-06-24 17:36:39 +0530

    chore: one to many relationship with posts

--- script.ts
+++ script.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const user = await prisma.user.findMany({});
+  const user = await prisma.user.findMany();
 
   console.log(user);
 }
