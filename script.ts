82e7fe937daf090605fd22960472b478d1e79ce8
swapnil-talpade <swapniltalpade41@gmail.com>
2023-06-25 15:15:33 +0530

    chore: client update ops

--- script.ts
+++ script.ts
@@ -2,23 +2,38 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  //   await prisma.user.deleteMany();
-  const user = await prisma.user.findMany({
-    where: {
-      AND: [
-        {
-          name: {
-            contains: "h",
-          },
-        },
-        {
-          email: {
-            contains: "@gmail.com",
-          },
-        },
-      ],
-    },
-  });
+//   await prisma.user.deleteMany();
+//   const user = await prisma.user.createMany({
+//     data: [
+//       {
+//         name: "swapnil",
+//         email: "swapnil@gmail.com",
+//         age: 45,
+//       },
+//       {
+//         name: "harshal",
+//         email: "harshal@gmail.com",
+//         age: 23,
+//       },
+//       {
+//         name: "rahul",
+//         email: "rahul@gmail.com",
+//         age: 50,
+//       },
+//     ],
+//   });
+
+    // const user = await prisma.user.updateMany({
+    //   where: {
+    //     name: "swapnil",
+    //   },
+    //   data: {
+    //     name: "sahil",
+    //     email: "sahil@gmail.com",
+    //   },
+    // });
+
+  const user = await prisma.user.findMany();
 
   console.log({ user });
 }
